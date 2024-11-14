7dee315369648b3174a054a74f6a3f9a35f5eb50
Yüksel Berk TARZAN <y.berktarzan@gmail.com>
2024-11-14 11:54:38 +0300

    First init commands and service.
    First init commands and service.

---
+++ src/MokaService.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace 4Rival\MokaLaravel;
+
+class MokaService
+{
+protected string $dealerCode;
+protected string $username;
+protected string $password;
+protected $checkKey;
+
+public function __construct()
+{
+$this->dealerCode = $this->config('services.moka.dealer_code');
+$this->username = config('services.moka.username');
+$this->password = config('services.moka.password');
+$this->checkKey = $this->generateCheckKey();
+}
+
+private function generateCheckKey(): string
+{
+$checkString = $this->dealerCode . 'MK' . $this->username . 'PD' . $this->password;
+return hash('sha256', $checkString);
+}
+
+public function getCheckKey(): string
+{
+return $this->checkKey;
+}
+
+
+}
